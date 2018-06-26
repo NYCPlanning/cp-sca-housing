9dd46f11f22b2d0451ca770895f0e02e8c296d7f
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-06-26 18:22:26 -0400

    Update 3b_hpd_export.sql

--- 3b_hpd_export.sql
+++ 3b_hpd_export.sql
@@ -6,7 +6,7 @@ ADD COLUMN geo_subdist text;
 UPDATE capitalplanning.hpd_2018_sca_inputs_geo_pts
 SET geo_subdist = b.distzone
 FROM dcpadmin.doe_schoolsubdistricts AS b
-WHERE ST_Intersects(hpd_2018_sca_inputs_geo_pts_copy.the_geom, b.the_geom)
+WHERE ST_Intersects(hpd_2018_sca_inputs_geo_pts.the_geom, b.the_geom)
 
 -- Check
 SELECT * FROM capitalplanning.hpd_2018_sca_inputs_geo_pts
@@ -21,5 +21,5 @@ SELECT
   geo_subdist,
   ST_X(the_geom) AS latitude, ST_Y(the_geom) AS longitude, bbl AS hpd_bbl, CONCAT(house_number,' ',street_name) AS address, borough
 FROM 
-    capitalplanning.hpd_2018_sca_inputs_geo_pts_copy
+    capitalplanning.hpd_2018_sca_inputs_geo_pts
 ORDER BY status, project_id, building_id
