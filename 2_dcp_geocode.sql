cea012bb6dbbab0b70a10e6aa6b5c6bf61563c13
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-06-25 22:29:01 -0400

    Update 2_dcp_geocode.sql

--- 2_dcp_geocode.sql
+++ 2_dcp_geocode.sql
@@ -27,6 +27,38 @@ WHERE ulurpno = temp.ulurp
 
 /**Repeat above for other boroughs**/
 
+/**Identify projects in nyzma that did not automatically match to project IDs**/
+SELECT * FROM capitalplanning.nyzma_may2018
+WHERE project_id is null
+
+/**Manually add in project IDs that are in nyzma but did not match by ulurpno**/
+-- *Note that theoretically this should not be occur as ZAP should've recorded all ulurp numbers associated with a project
+
+UPDATE capitalplanning.nyzma_may2018
+SET project_id = 'P2012K0184'
+WHERE project_na = 'MILL BASIN' 
+AND project_id is null;
+
+UPDATE capitalplanning.nyzma_may2018
+SET project_id = 'P2016M0065'
+WHERE project_na = 'Inwood Rezoning' 
+AND project_id is null;
+
+UPDATE capitalplanning.nyzma_may2018
+SET project_id = 'P2015X0380'
+WHERE project_na = 'Westchester Mews Rezoning' 
+AND project_id is null;
+
+UPDATE capitalplanning.nyzma_may2018
+SET project_id = 'P2014K0163'
+WHERE project_na like 'Hamilton%Patio' 
+AND project_id is null;
+
+UPDATE capitalplanning.nyzma_may2018
+SET project_id = 'P2012M0224'
+WHERE project_na = 'RIVER PLAZA REZONING' 
+AND project_id is null
+
 /**Geocode to nyzma first, then to imPACT Visualization polygons**/
 
 ALTER TABLE capitalplanning.all_possible_projects
@@ -49,7 +81,29 @@ AND all_possible_projects.the_geom is null;
 
 UPDATE capitalplanning.all_possible_projects
 SET geom_source = 'imPACT Visualization'
-WHERE the_geom is not null AND geom_source is null
+WHERE the_geom is not null AND geom_source is null;
+
+UPDATE capitalplanning.all_possible_projects
+SET the_geom = z.the_geom
+FROM capitalplanning.nyzma_may2018 AS z
+WHERE project_name = 'Melrose Commons North RFP Site B'
+AND z.ulurpno = '080002zmx';
+
+UPDATE capitalplanning.all_possible_projects
+SET geom_source = 'Manual, nyzma'
+FROM capitalplanning.nyzma_may2018 AS z
+WHERE project_name = 'Melrose Commons North RFP Site B'
+
+UPDATE capitalplanning.all_possible_projects
+SET the_geom = z.the_geom
+FROM capitalplanning.nyzma_may2018 AS z
+WHERE all_possible_projects.project_id = '2018X0371'
+AND z.ulurpno = '180131zmx';
+
+UPDATE capitalplanning.all_possible_projects
+SET geom_source = 'Manual, nyzma'
+FROM capitalplanning.nyzma_may2018 AS z
+WHERE all_possible_projects.project_id = '2018X0371'
 
 /**Identify projects with missing geometries**/
 SELECT * FROM capitalplanning.all_possible_projects
