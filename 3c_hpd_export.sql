86c6e64105b1a28df48ff47510a1400e10ac60db
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-06-26 17:09:06 -0400

    Update 3c_hpd_export.sql

--- 3c_hpd_export.sql
+++ 3c_hpd_export.sql
@@ -5,7 +5,7 @@ SELECT
   status, project_start_date, building_completion_date, projected_start_date, projected_completion_date, total_units,
   match_method, dob_job_number, dob_u_matched, incremental_hpd_units,
   geo_subdist,
-  ST_X(the_geom) AS latitude, ST_Y(the_geom) AS longitude, bbl AS hpd_bbl, CONCAT(house_number,' ',street_name) AS address, borough,
+  ST_X(the_geom) AS latitude, ST_Y(the_geom) AS longitude, bbl AS hpd_bbl, CONCAT(house_number,' ',street_name) AS address, borough
 FROM 
     capitalplanning.hpd_2018_sca_inputs_geo_pts
 ORDER BY project_id, building_id
