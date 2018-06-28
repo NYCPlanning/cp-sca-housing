fb7f93ca32ee75bccd3f37d0e73072d6cd896c68
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-06-28 18:48:36 -0400

    Create 5b_dcp_subdist_split.sql

---
+++ 5b_dcp_subdist_split.sql
@@ -0,0 +1,11 @@
+/**Calculate proportion of project in subdistrict**/
+
+SELECT 
+  	p.the_geom, p.the_geom_webmercator, p.project_id, p.project_name, b.distzone, 
+ROUND(CAST(ST_Area(ST_Intersection(p.the_geom::geography,b.the_geom::geography))/ST_Area(p.the_geom::geography)*100 AS DECIMAL),0) AS pct_overlap
+FROM 
+capitalplanning.all_possible_projects AS p,
+dcpadmin.doe_schoolsubdistricts AS b
+WHERE ST_Intersects(p.the_geom, b.the_geom)
+AND manual_exclude is null
+ORDER BY project_id, pct_overlap
