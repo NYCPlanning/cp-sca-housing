ea8a0260ed7db4c5de2179a22d49b5e07a047467
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-07-02 15:25:16 -0400

    Create 5c_dcp_boro_inputs.sql

---
+++ 5c_dcp_boro_inputs.sql
@@ -0,0 +1,6 @@
+/**Add exclusions identified in 4b_dcp_dob_manual_edits.sql**/
+
+/**Add in borough planner inputs on likelihood to be built and rationale**/
+
+/**Clean up project status to ensure properly marked as Active or Complete**/
+
