485dc94b46ab012e942acbe623932e656d5a8697
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-07-02 18:58:51 -0400

    Update 6a_cityareawide_data_prep.sql

--- 6a_cityareawide_data_prep.sql
+++ 6a_cityareawide_data_prep.sql
@@ -5,7 +5,12 @@
 /**Identify adopted city-initiated area-wide rezonings since 2000**/
 -- *Chose 2000 onwards because build years are typically 10-15 years. However old Planning Coordination only dates back to 2003. Confirmed that no major rezonings are missing
 
-WITH projects AS (
+WITH unioned AS (
+SELECT ST_Union(z.the_geom) AS the_geom, z.project_id, z.ulurpno, z.project_na, z.status, z.effective
+FROM capitalplanning.nyzma_may2018 AS z
+GROUP BY z.project_id, z.ulurpno, z.project_na, z.status, z.effective),
+
+projectdata AS (
 SELECT * FROM capitalplanning.bx_projects
 UNION
 SELECT * FROM capitalplanning.bK_projects
@@ -16,23 +21,22 @@ SELECT * FROM capitalplanning.qn_projects
 UNION
 SELECT * FROM capitalplanning.si_projects),
 
-cityled AS (
-SELECT z.the_geom, z.project_id, z.ulurpno, z.project_na, z.status, z.effective,
-p.applicant_type, p.project_description, p.lead_action, p.new_dwelling_units, p.anticipated_year_built, p.total_dwelling_units_in_project
-FROM capitalplanning.nyzma_may2018 AS z
-INNER JOIN projects AS p
-ON z.project_id = p.project_id
-WHERE EXTRACT(YEAR FROM z.effective) >= 2000
+cityledall AS (
+SELECT
+    unioned.the_geom, unioned.project_id, unioned.ulurpno, unioned.project_na, unioned.status, unioned.effective,
+    p.applicant_type, p.project_description, p.lead_action, p.new_dwelling_units, p.total_dwelling_units_in_project, p.anticipated_year_built
+FROM unioned    
+INNER JOIN projectdata AS p
+ON unioned.project_id = p.project_id
+WHERE EXTRACT(YEAR FROM unioned.effective) >= 2000
 AND p.applicant_type = 'DCP' OR p.applicant_type = 'Other Public Agency'
 ORDER BY ulurpno)
 
-SELECT
-cityled.the_geom, cityled.project_id, cityled.ulurpno, project_na, status, effective, cityled.applicant_type, project_description, lead_action, new_dwelling_units, total_dwelling_units_in_project, anticipated_year_built, 
-s.projected_units, s.build_year
-FROM cityled
+SELECT cityledall.*, s.projected_units, s.build_year
+FROM cityledall
 INNER JOIN capitalplanning.old_sca_report_with_units AS s
-ON cityled.ulurpno = LOWER(s.ulurpno)
-AND (s.projected_units >= 200 OR GREATEST(total_dwelling_units_in_project, new_dwelling_units) >= 200)
+ON cityledall.ulurpno = LOWER(s.ulurpno)
+AND (s.projected_units >= 200 OR GREATEST(cityledall.total_dwelling_units_in_project, cityledall.new_dwelling_units) >= 200)
 
 -- Create new dataset from query as cityled_projects
 
@@ -44,4 +48,6 @@ ADD COLUMN in_site_specific numeric;
 UPDATE capitalplanning.cityled_projects
 SET in_site_specific = 1
 WHERE project_id IN (SELECT DISTINCT project_id FROM capitalplanning.all_possible_projects WHERE manual_exclude is null)
+
+
 
