cd399bade0dbf6e908b11d1aa522604c14433cb0
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-07-02 10:40:44 -0400

    Update 4d_dcp_hpd_dedupe.sql

--- 4d_dcp_hpd_dedupe.sql
+++ 4d_dcp_hpd_dedupe.sql
@@ -1,4 +1,38 @@
--- Double check if any projects in DCP that appear to be HPD-financed were not matched (*Creston Ave, adAPT, Maple Mesa should’ve been in HPD data, but already captured in permits. Bedford Arms & 126th Bus Depot are EDC. ECD likely too early to be in HPD)
+/**Develop deduping methodology**/
+-- *Note that matching solely based on spatial overlap. Timing not placed as a matching criteria due to high overlap in project timelines and lack of predictability in diff between DCP project completion & HPD project start dates 
+
+SELECT 
+    	i.project_id, i.project_name, project_completed, certified_referred, units,
+	h.project_id AS hpd_project_id, h.building_id, h.project_name AS hpd_project_name, status, total_units
+FROM 
+    capitalplanning.all_possible_projects AS i
+LEFT JOIN
+	capitalplanning.hpd_2018_sca_inputs_geo_pts AS h
+ON ST_Intersects(h.the_geom,i.the_geom)
+WHERE manual_exclude is null
+ORDER BY i.project_id, hpd_project_id
+
+/**Manually add in missing**/
+-- *One of the two HPD financed Spofford buildings outside of DCP project area
+
+INSERT INTO capitalplanning.dcp_hpd_dedupe (project_id, hpd_project_id, building_id)
+SELECT a.project_id, a.hpd_project_id::numeric, a.hpd_building_id::numeric
+FROM capitalplanning.dcp_hpd_dedupe_add AS a
+WHERE CONCAT(a.project_id,a.hpd_project_id,a.hpd_building_id) NOT IN (SELECT DISTINCT CONCAT(project_id,hpd_project_id,building_id) FROM capitalplanning.dcp_hpd_dedupe);
+
+UPDATE capitalplanning.dcp_hpd_dedupe
+SET project_name = i.project_name, units = i.units, project_completed = i.project_completed, certified_referred = i.certified_referred
+FROM capitalplanning.all_possible_projects AS i
+WHERE dcp_hpd_dedupe.project_id = i.project_id;
+
+UPDATE capitalplanning.dcp_hpd_dedupe
+SET hpd_project_name = h.project_name, status = h.status, total_units = h.total_units
+FROM capitalplanning.hpd_2018_sca_inputs_geo_pts AS h
+WHERE dcp_hpd_dedupe.hpd_project_id = h.project_id AND dcp_hpd_dedupe.building_id = h.building_id
+
+-- Double check if any projects in DCP that appear to be HPD-financed were not matched
+-- Results
+	-- Creston Ave, adAPT, Maple Mesa should’ve been in HPD data, but manual match not needed bc already fully matched to permits. Bedford Arms & 126th Bus Depot are EDC
 
 WITH exact AS (
 SELECT 
@@ -19,31 +53,3 @@ WHERE (project_description LIKE '%HPD%' OR project_description LIKE '%hpd%' OR a
 AND project_completed is null
 GROUP BY project_id, project_name, project_status, units
 ORDER BY remaining_units, units ASC
-
---- Create deduped sheet
-SELECT 
-    	i.project_id, i.project_name, project_completed, certified_referred, units,
-	h.project_id AS hpd_project_id, h.building_id, h.project_name AS hpd_project_name, status, total_units
-FROM 
-    capitalplanning.all_possible_projects AS i
-LEFT JOIN
-	capitalplanning.hpd_2018_sca_inputs_geo_pts AS h
-ON ST_Intersects(h.the_geom,i.the_geom)
-WHERE manual_exclude is null
-ORDER BY i.project_id, hpd_project_id
-
---- Add any missing (Spofford), bring into main deduping sheet
-INSERT INTO capitalplanning.dcp_hpd_dedupe (project_id, hpd_project_id, building_id)
-SELECT a.project_id, a.hpd_project_id::numeric, a.hpd_building_id::numeric
-FROM capitalplanning.dcp_hpd_dedupe_add AS a
-WHERE CONCAT(a.project_id,a.hpd_project_id,a.hpd_building_id) NOT IN (SELECT DISTINCT CONCAT(project_id,hpd_project_id,building_id) FROM capitalplanning.dcp_hpd_dedupe);
-
-UPDATE capitalplanning.dcp_hpd_dedupe
-SET project_name = i.project_name, units = i.units, project_completed = i.project_completed, certified_referred = i.certified_referred
-FROM capitalplanning.all_possible_projects AS i
-WHERE dcp_hpd_dedupe.project_id = i.project_id;
-
-UPDATE capitalplanning.dcp_hpd_dedupe
-SET hpd_project_name = h.project_name, status = h.status, total_units = h.total_units
-FROM capitalplanning.hpd_2018_sca_inputs_geo_pts AS h
-WHERE dcp_hpd_dedupe.hpd_project_id = h.project_id AND dcp_hpd_dedupe.building_id = h.building_id
