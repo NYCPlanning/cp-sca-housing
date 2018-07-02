8b99aef539c4ea67c7f0b6462bacf338a3119f0a
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-07-02 15:22:19 -0400

    Update 5b_dcp_subdist_split.sql

--- 5b_dcp_subdist_split.sql
+++ 5b_dcp_subdist_split.sql
@@ -9,3 +9,27 @@ dcpadmin.doe_schoolsubdistricts AS b
 WHERE ST_Intersects(p.the_geom, b.the_geom)
 AND manual_exclude is null
 ORDER BY project_id, pct_overlap
+
+-- Create new dataset from query as dcp_subdist_distribute
+
+ALTER TABLE capitalplanning.dcp_subdist_distribute
+ADD COLUMN temp_pct numeric,
+ADD COLUMN temp_sum numeric,
+ADD COLUMN final_pct_overlap numeric;
+
+-- Check if any outside of subdistricts (for example, if DCP polygon extends in water)
+
+WITH summed as (
+SELECT project_id, project_name, SUM(pct_overlap) AS pct_overlap FROM capitalplanning.dcp_subdist_distribute
+GROUP BY project_id, project_name
+ORDER BY pct_overlap ASC)
+
+update capitalplanning.dcp_subdist_distribute
+set temp_sum = summed.pct_overlap
+from summed where dcp_subdist_distribute.project_id = summed.project_id;
+
+update capitalplanning.dcp_subdist_distribute
+set temp_pct = pct_overlap/temp_sum;
+
+update capitalplanning.dcp_subdist_distribute
+set final_pct_overlap = decimal(temp_pct * 100,0)
