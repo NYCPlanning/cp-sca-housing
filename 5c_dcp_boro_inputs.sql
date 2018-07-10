ae5217b2d9b418f24891b6a592bf2d56a0de6e87
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-07-10 19:57:25 -0400

    Update 5c_dcp_boro_inputs.sql

--- 5c_dcp_boro_inputs.sql
+++ 5c_dcp_boro_inputs.sql
@@ -79,13 +79,33 @@ SET manual_exclude = 0, reason_for_excluding = 'Originally conceived as all seni
 WHERE project_id = 'P2012R0625'
 
 /**Add in final inputs from DCP boroughs**/
+-- Adding in P2017R0111 (uncertain why it was not captured automatically) and P2018R0035 (not picked up bc no units filled & no indicators in text description)
+-- Deduping not needed bc both projects are on-hold
+
+INSERT INTO capitalplanning.all_possible_projects (borough, community_districts, lead_planner, project_id, project_name, project_description, lead_action, project_status, process_stage, project_completed, certified_referred, system_target_certification_date, applicant_type, new_dwelling_units, total_dwelling_units_in_project)
+SELECT borough, community_districts, lead_planner, project_id, project_name, project_description, lead_action, project_status, process_stage_name_stage_id_process_stage, project_completed, certified_referred, system_target_certification_date, applicant_type, new_dwelling_units, total_dwelling_units_in_project FROM si_projects
+WHERE project_id = 'P2017R0111' OR project_id = 'P2018R0035';
+
+UPDATE capitalplanning.all_possible_projects
+SET the_geom = i.the_geom, geom_source = 'imPACT Visualization'
+FROM capitalplanning.impact_poly_latest AS i
+WHERE project_id = 'P2017R0111' OR project_id = 'P2018R0035'
+
 -- Adjusting likelihood to be built
+UPDATE capitalplanning.all_possible_projects
+SET likely_to_be_built = c.remaining_units_likely_to_be_built, rationale = c.rationale
+FROM capitalplanning.project_info_cleaning_072018 AS c
+WHERE all_possible_projects.project_id = c.project_id;
+
 UPDATE capitalplanning.all_possible_projects
 SET likely_to_be_built = c.remaining_units_likely_to_be_built, rationale = c.rationale
 FROM capitalplanning.project_info_cleaning_072018 AS c
 WHERE all_possible_projects.project_id = c.project_id
 
--- Adding in P2017R0111 (uncertain why it was not captured automatically)
-INSERT INTO capitalplanning.all_possible_projects (borough, community_districts, lead_planner, project_id, project_name, project_description, lead_action, project_status, process_stage, project_completed, certified_referred, system_target_certification_date, applicant_type, new_dwelling_units, total_dwelling_units_in_project)
-SELECT borough, community_districts, lead_planner, project_id, project_name, project_description, lead_action, project_status, process_stage_name_stage_id_process_stage, project_completed, certified_referred, system_target_certification_date, applicant_type, new_dwelling_units, total_dwelling_units_in_project FROM si_projects
-WHERE project_id = 'P2017R0111'
+-- Adjusting manual exclusions
+UPDATE capitalplanning.all_possible_projects
+SET manual_exclude = c.manual_exclude::numeric, reason_for_excluding = c.reason_for_excluding
+FROM capitalplanning.project_info_cleaning_072018 AS c
+WHERE all_possible_projects.project_id = 'P2018R0035'
+AND all_possible_projects.project_id = c.project_id
+
