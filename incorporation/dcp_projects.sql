48d310a350ee6a58755a93cc2743151cef318f86
mqli322 <30453673+mqli322@users.noreply.github.com>
2018-07-19 11:47:00 -0400

    Update dcp_projects.sql

--- incorporation/dcp_projects.sql
+++ incorporation/dcp_projects.sql
@@ -5,6 +5,27 @@ FROM capitalplanning.bx_actions
 WHERE project IN (SELECT DISTINCT project_id FROM capitalplanning.all_possible_projects)
 AND project_action_status IN ('Active','Certified','Referred','Approved')
 GROUP BY project
-ORDER BY project
-
--- Repeat for all 5 boroughs
+UNION
+SELECT project, count(action_code) AS num_valid_actions, count(CASE WHEN action_code = 'ZM' THEN 1 END) AS zm_action
+FROM capitalplanning.bK_actions
+WHERE project IN (SELECT DISTINCT project_id FROM capitalplanning.all_possible_projects)
+AND project_action_status IN ('Active','Certified','Referred','Approved')
+GROUP BY project
+UNION
+SELECT project, count(action_code) AS num_valid_actions, count(CASE WHEN action_code = 'ZM' THEN 1 END) AS zm_action
+FROM capitalplanning.mn_actions
+WHERE project IN (SELECT DISTINCT project_id FROM capitalplanning.all_possible_projects)
+AND project_action_status IN ('Active','Certified','Referred','Approved')
+GROUP BY project
+UNION
+SELECT project, count(action_code) AS num_valid_actions, count(CASE WHEN action_code = 'ZM' THEN 1 END) AS zm_action
+FROM capitalplanning.qn_actions
+WHERE project IN (SELECT DISTINCT project_id FROM capitalplanning.all_possible_projects)
+AND project_action_status IN ('Active','Certified','Referred','Approved')
+GROUP BY project
+UNION
+SELECT project, count(action_code) AS num_valid_actions, count(CASE WHEN action_code = 'ZM' THEN 1 END) AS zm_action
+FROM capitalplanning.si_actions
+WHERE project IN (SELECT DISTINCT project_id FROM capitalplanning.all_possible_projects)
+AND project_action_status IN ('Active','Certified','Referred','Approved')
+GROUP BY project
